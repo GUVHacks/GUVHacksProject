a6d5f77140c816a0d2fa68208e00ef5399f0caf0
YanchenWangGU <32420444+YanchenWangGU@users.noreply.github.com>
2018-03-10 02:03:31 +0100

    update credit

--- frank/defaultModel.py
+++ frank/defaultModel.py
@@ -1,80 +1,61 @@
 import random
 
 class applicant:
-    # creditScore: int: credit score of the applicant
     # curEmploy: Boolean. If the person is currently employeed, True
     # curIncMo: int or float. if currently employeed, the monthly salary 
     # bankBal: int or float: money in the bank account
-    # full_time: boolean: if the person has full time job
     # reminTerm: int: if there person is contracted, how many months remaining for the contract
-    # avgInc: int or float: average income 
-    def __init__(self,creditScore,curEmploy,curIncMo,bankBal,full_time,remainTerm,avgInc):
-        self.creditScore = creditScore
+    # avgInc: int or float: the income from the most recent job (previous job)
+    def __init__(self,curEmploy,curIncMo,bankBal,remainTerm,avgInc):
         self.curIncMo= curIncMo
+        self.curEmploy = curEmploy
         self.bankBal = bankBal
-        self.full_time = full_time
         self.remainTerm = remainTerm
-        if full_time:
-            self.remainTerm = 12
         self.curIncMo = curIncMo
         if not curEmploy:
             self.curIncMo = avgInc
         self.avgInc = avgInc
-        
     
-#def defaultRatio(applicantList,rent,employment):
-##    df = pd.read_csv('EUEmployment.csv')
-#    indivRent = rent/len(applicantList)
-#    
-#    avgCreditScore = np.mean([i.creditScore for i in applicantList])
-#    print(avgCreditScore)
-#    for app in applicantList: 
-#        groupBal = groupBal+ app.bankBal
-#    begBal = groupBal
-#    for i in range(12):
-#        for app in applicantList: 
-#            fixedExp = indivRent*2
-#            if app.remainTerm ==0:
-#                newJob = np.random.binomial(1,.7)
-#                # applicant get the new job 
-#                if newJob==1:
-#                    app.remainTerm = np.random.randint(0,high = 3)
-#                    app.bankBal = app.bankBal + app.curIncMo - fixedExp
-#                # the case that applicant doesn't get the new job
-#                else:
-#                    app.bankBal = app.bankBal - fixedExp
-#            else:
-#                app.bankBal = app.bankBal + app.curIncMo - fixedExp
-#                app.remainTerm  = app.remainTerm - 1
-#            if app.bankBal < 0:
-#                app.defaultTime = app.defaultTime +1
-#                print('gg at month', i +1)
-#        groupBal = 0
-#        for app in applicantList: 
-#            groupBal = groupBal+ app.bankBal
-#        if groupBal <0:
-#            print('group default')
-#        print(groupBal)
-#    
-#    print('diff', groupBal - begBal)
-#    
-#        
-#a1 = applicant(80,True,1000,200,False,1,900)
-#
-#defaultRatio([a1],300,'Italy')
-#
 
+def getDeepCopy(app):
+    return applicant(app.curEmploy,app.curIncMo,app.bankBal,
+                       app.remainTerm,app.avgInc)
     
-                
-def individualDefaultRatio(app,rent,employment):
-#    df = pd.read_csv('EUEmployment.csv')
-    result = [0]*12
-    for j in range(1000):
+
+# Param:
+    # applicant: the applicant created in the applicant class
+    # rent: int, the rent for the new apartment
+    # employment: probability to get a job within a month. By default, 
+    # it is 0.7 unless user change it
+    
+# Output:
+    # a list. item 0-11 are the default rate for a given month. e.g. the third 
+    # one (index 2) is the default rate for the applicant at month 3
+    #
+    # list[12] is the average amount of money the applicant would have in the bank account
+    # list[11] is the probabilty the applicant has a bank account balance below 0. 
+    # i.e. real default. This is same as the default rate at the end of month 12
+    
+# Assumptions: The rent is 30% of the applicant's income and other expenses  
+# have the same amount as rent in total. i.e. the applicant will speed 60% of income per month
+# By default, the employment rate is 0.7 i.e. the probability for the applicant 
+# to get a job with in a month is 0.7
+    
+# Methodology: Use simulations with randomization to simulate real life possiblities.
+# When the applicant ends the contract, the model uses bernoulli trial with 
+# p = employment. If the outcome is 1, the applicant gets a new job and to simulate 
+# the real lifethe length/contract for the new job is either 1 or 2 or 3 or 4 
+# months with the equal probability 
+
+def individualDefaultRatio(applicant,rent,employment=.7):
+    result = [0]*13
+    for j in range(10000):
+        app = getDeepCopy(applicant)
         for i in range(12):
             fixedExp = rent*2
             if app.remainTerm ==0:
-                newJob = 1 if random.uniform(0,1)<.7 else 0
-                # applicant get the new job 
+                newJob = 1 if random.uniform(0,1)<employment else 0
+                # applicant get the new job, the job is 
                 if newJob==1:
                     app.remainTerm = random.randint(0,3)
                     app.bankBal = app.bankBal + app.curIncMo - fixedExp
@@ -86,7 +67,16 @@ def individualDefaultRatio(app,rent,employment):
                 app.remainTerm  = app.remainTerm - 1
             if app.bankBal < 0:
                 result[i] = result[i]+1
+        result[12] = app.bankBal+result[12]
+    result =  [i/10000 for i in result]
+    
     return result
 
-a1 = applicant(80,True,1000,200,False,1,900)
-individualDefaultRatio(a1,)
+########### example call. DO NOT INCLUDE IN THE CODE ###########
+a1 = applicant(True,1000,200,2,900)
+individualDefaultRatio(a1,300)
+########### END OF EXAMPLE CALL ########################
+
+# For landlord, the only last result[11] is needed for landlord
+
+# For refugees and guaranteurs, they have the access for all information
